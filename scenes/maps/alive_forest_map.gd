c4e0217172d79cf7c37e52e62260f8325ba8217e
Carrie <cwaz2017@gmail.com>
2024-04-18 14:31:05 -0400

    add background music to all maps

---
+++ scenes/maps/alive_forest_map.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+@onready var BG_music_forest = $"AudioStream_alive_forest"
+var BG_music_on =true
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	update_music_status()
+	
+func update_music_status():
+	if BG_music_on:
+		if !BG_music_forest.playing:
+			BG_music_forest.play()
+	else:
+		BG_music_forest.stop()
