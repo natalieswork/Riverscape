788850f7cacea707d9248b694e5a6df7e3c6ccab
Carrie <cwaz2017@gmail.com>
2024-04-22 17:37:23 -0400

    added cut out background music when encounter starts to dying and alive map

--- scenes/maps/alive_forest_map.gd
+++ scenes/maps/alive_forest_map.gd
@@ -1,14 +1,20 @@
 extends Node2D
-
-
 @onready var BG_music_forest = $"AudioStream_alive_forest"
 var BG_music_on =true
 
+@onready var coyote = $"Coyote"
+@onready var encounter_alive = $"AudioStream_encounter_alive"
+var encounter_music_on = false
+signal encounter_music_started
+signal encounter_music_stopped
+
+func _ready():
+	$coyote.connect("encounter_music_started", _on_encounter_music_started)
+	$coyote.connect("encounter_music_stopped", _on_encounter_music_stopped)
 
 # Called when the node enters the scene tree for the first time.
 #func _ready():
 	#pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,7 +22,32 @@ func _process(delta):
 	
 func update_music_status():
 	if BG_music_on:
+		encounter_alive.stop()
 		if !BG_music_forest.playing:
 			BG_music_forest.play()
 	else:
 		BG_music_forest.stop()
+		
+	if encounter_music_on:
+		BG_music_forest.stop()
+		if !encounter_alive.playing:
+			encounter_alive.play()
+	else:
+		encounter_alive.stop()
+func _on_encounter_music_started():
+	print("music")
+	BG_music_on = false
+	encounter_music_on= true
+
+func _on_encounter_music_stopped():
+	print("stoppy")
+	BG_music_on = true
+	encounter_music_on = false
+
+
+func _on_coyote_encounter_music_started():
+	_on_encounter_music_started()
+
+
+func _on_coyote_encounter_music_stopped():
+	_on_encounter_music_stopped()
--- scenes/maps/dying_forest_map.gd
+++ scenes/maps/dying_forest_map.gd
@@ -1,22 +1,49 @@
 extends Node2D
 
-
-@onready var BG_music_forest = $"AudioStream_dead_forest"
+@onready var BG_music_forest_dying = $"AudioStream_dead_forest"
 var BG_music_on =true
+@onready var coyote = $"Coyote"
+@onready var encounter = $"AudioStream_encounter"
+var encounter_music_on = false
+signal encounter_music_started
+signal encounter_music_stopped
 
-
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
-
-
+func _ready():
+	$coyote.connect("encounter_music_started", _on_encounter_music_started)
+	$coyote.connect("encounter_music_stopped", _on_encounter_music_stopped)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	update_music_status()
 	
 func update_music_status():
 	if BG_music_on:
-		if !BG_music_forest.playing:
-			BG_music_forest.play()
+		encounter.stop()
+		if !BG_music_forest_dying.playing:
+			BG_music_forest_dying.play()
 	else:
-		BG_music_forest.stop()
+		BG_music_forest_dying.stop()
+		
+	if encounter_music_on:
+		BG_music_forest_dying.stop()
+		if !encounter.playing:
+			encounter.play()
+	else:
+		encounter.stop()
+
+func _on_encounter_music_started():
+	print("music")
+	BG_music_on = false
+	encounter_music_on= true
+
+func _on_encounter_music_stopped():
+	print("stoppy")
+	BG_music_on = true
+	encounter_music_on = false
+
+func _on_coyote_encounter_music_started():
+	_on_encounter_music_started()
+
+
+func _on_coyote_encounter_music_stopped():
+	_on_encounter_music_stopped()
