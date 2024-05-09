96c84fd63a1901a7227a544ec4eefc2e26f21c37
Carrie <cwaz2017@gmail.com>
2024-05-09 12:15:17 -0400

    add winner audio

--- scripts/ui/winner.gd
+++ scripts/ui/winner.gd
@@ -1,13 +1,17 @@
 extends CanvasLayer
 @onready var player_inv: Inv = preload("res://scenes/inventory/player_inventory.tres")
+@onready var bg_music = $Audio/AudioStream_music
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if !bg_music.playing:
+			bg_music.play()
 	global.new_game_stats()
 	player_inv.reset()
 	$AnimationPlayer.play("float_end")
 	await get_tree().create_timer(3).timeout
 	$AnimationPlayer.play("exit_text")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
